ffcb06bcd6bfa14490099789bd6cd6f9c2289c84
LegionDroid <brunolegion@hotmail.com>
2019-12-19 22:12:03 -0300

    criação do ViewModel com mock

--- app/src/main/java/com/brfdeveloper/countries/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/brfdeveloper/countries/viewmodel/ListViewModel.kt
@@ -1,6 +1,35 @@
 package com.brfdeveloper.countries.viewmodel
 
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
+import com.brfdeveloper.countries.model.Country
 
 class ListViewModel: ViewModel() {
+
+    val countries = MutableLiveData<List<Country>>()
+    val countryLoadError = MutableLiveData<Boolean>()
+    val loading = MutableLiveData<Boolean>()
+
+    fun refresh(){
+        fetchData()
+    }
+
+    private fun fetchData() {
+        val mockData = listOf(
+            Country("CountryA"),
+            Country("CountryB"),
+            Country("CountryC"),
+            Country("CountryD"),
+            Country("CountryE"),
+            Country("CountryF"),
+            Country("CountryG"),
+            Country("CountryH"),
+            Country("CountryI")
+            )
+
+        countryLoadError.value = false
+        loading.value = false
+        countries.value = mockData
+    }
+
 }
