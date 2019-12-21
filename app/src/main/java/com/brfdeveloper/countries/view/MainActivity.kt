420417f1b317c77bcd1f941c393bab8b29774ba2
LegionDroid <brunolegion@hotmail.com>
2019-12-21 02:16:25 -0300

    criação da MainActivity e adicionado o viewModel nela

--- app/src/main/java/com/brfdeveloper/countries/view/MainActivity.kt
+++ app/src/main/java/com/brfdeveloper/countries/view/MainActivity.kt
@@ -1,13 +1,61 @@
 package com.brfdeveloper.countries.view
 
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModel
+import android.arch.lifecycle.ViewModelProviders
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.view.View
+import android.widget.LinearLayout
 import com.brfdeveloper.countries.R
+import com.brfdeveloper.countries.viewmodel.ListViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var viewModel: ListViewModel
+    private val countriesAdapter = CountryListAdapter(arrayListOf())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel = ViewModelProviders.of(this).get(ListViewModel::class.java)
+        viewModel.refresh()
+
+        actmain_rv_countriesList.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = countriesAdapter
+        }
+
+        observeViewModel()
+    }
+
+    fun observeViewModel() {
+
+        viewModel.countries.observe(this, Observer { countries ->
+            countries?.let {
+                countriesAdapter.updateCountries(it)
+            }
+        })
+
+        viewModel.countryLoadError.observe(this, Observer { loadError ->
+            loadError?.let {
+                actmain_tv_list_error.visibility = if (it) View.VISIBLE else View.GONE
+            }
+        })
+
+        viewModel.loading.observe(this, Observer { isLoading ->
+            isLoading?.let {
+                actmain_pb.visibility = if (it) View.VISIBLE else View.GONE
+                if(it){
+                    actmain_tv_list_error.visibility = View.GONE
+                    actmain_rv_countriesList.visibility = View.GONE
+                }
+            }
+        })
+
+
     }
 }
